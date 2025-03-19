262f7d7d77402df57c3e17b0a7ff8b625f8954f1
pht1412 <lhp141204@gmail.com>
2025-03-19 08:19:23 +0700

    primeshop

---
+++ BE-SpringBoot/src/main/java/com/primeshop/be_springboot/BeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.primeshop.be_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSpringbootApplication.class, args);
+	}
+
+}
